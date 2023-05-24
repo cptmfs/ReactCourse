d6c12db47df532f6234267cba691e462a4804aa7
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-05-24 15:37:23 +0300

    .

--- intro/src/CartSummary.js
+++ intro/src/CartSummary.js
@@ -11,33 +11,37 @@ import {
 
 export default class CartSummary extends Component {
     renderSummary() {
-        return(
-        <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
-                Your Cart
-            </DropdownToggle>
-            <DropdownMenu right>
-                {this.props.cart.map(cartItem => (
-                    <DropdownItem>{cartItem.product.productName}
-                        <Badge color="success">{cartItem.quantity}</Badge>
-                    </DropdownItem>
-                ))}
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-            </DropdownMenu>
-        </UncontrolledDropdown>
-    
-    );
-}
-renderEmptyCart(){
-    <NavItem>
-        <NavLink>Empty Cart</NavLink>
-    </NavItem>
-}
+        return (
+            <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                    Your Cart
+                </DropdownToggle>
+                <DropdownMenu right>
+                    {this.props.cart.map(cartItem => (
+                        <DropdownItem key={cartItem.product.id}>
+                            <Badge color='danger' onClick={() => this.props.removeFromCart(cartItem.product)}>X</Badge>
+                            {cartItem.product.productName}
+                            <Badge color="success">{cartItem.quantity}</Badge>
+                        </DropdownItem>
+                    ))}
+                    <DropdownItem divider />
+                    <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+            </UncontrolledDropdown>
+
+        );
+    }
+    renderEmptyCart() {
+        return (
+            <NavItem>
+                <NavLink>Empty Cart</NavLink>
+            </NavItem>
+        )
+    }
     render() {
         return <div>
-{this.props.cart.length>0?this.renderSummary():this.renderEmptyCart()}
+            {this.props.cart.length > 0 ? this.renderSummary() : this.renderEmptyCart()}
         </div>
-        ;
+            ;
     }
 }
