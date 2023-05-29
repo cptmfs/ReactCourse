97b5ea4dd3e32a5c8c3531e9b2430fb45fdb4893
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-05-29 22:16:36 +0300

    .

--- northwind-redux/src/components/categories/CategoryList.js
+++ northwind-redux/src/components/categories/CategoryList.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
-import {connect} from "react-redux"
+import { connect } from "react-redux"
+import { bindActionCreators } from 'redux';
+import * as categoryActions from '../../redux/actions/categoryActions'
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 class CategoryList extends Component {
+  componentDidMount() {
+    this.props.actions.getCategories()
+  }
   render() {
     return (
-      <div> 
-        <h3>CategoryList</h3> 
+      <div>
+        <h3>CategoryList {this.props.categories.length}</h3>
+        <ListGroup>
+          {
+            this.props.categories.map(category => (
+              <ListGroupItem key={category.id}>
+                {category.categoryName}
+              </ListGroupItem>
+            ))
+          }
+
+        </ListGroup>
         <h5> Seçili kategori :{this.props.currentCategory.categoryName}</h5>
-        </div>
+      </div>
     );
   }
 }
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    currentCategory:state.changeCategoryReducer
+    currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer
   }
 }
 
-export default connect(mapStateToProps)(CategoryList)
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(categoryActions.getCategories, dispatch)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList)
--- northwind-redux/src/redux/reducers/configureStore.js
+++ northwind-redux/src/redux/reducers/configureStore.js
@@ -1,6 +1,6 @@
-import {legacy_createStore} from "redux"
+import {applyMiddleware, legacy_createStore} from "redux"
 import rootReducer from "./index"
-
+import thunk from 'redux-thunk'
 export default function configureStore(){
-    return legacy_createStore(rootReducer)
+    return legacy_createStore(rootReducer,applyMiddleware(thunk))
 }
