946f425b7724e7c2b4be9a3208a8940db018a20a
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-05-29 14:10:03 +0300

    .

---
+++ northwind-redux/src/components/products/ProductList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class ProductList extends Component {
+  render() {
+    return (
+      <div> 
+        <h3>ProductList</h3> </div>
+    );
+  }
+}
